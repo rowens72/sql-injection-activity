f7a8cd31060fb85e8ea785cada73652582c6445a
Ron Owens <rowens72@yahoo.com>
2022-06-22 12:28:02 -0700

    no changes made to this file

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ path = require('path'),
 express = require('express'),
 bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
-
 const app = express();
-
 app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
